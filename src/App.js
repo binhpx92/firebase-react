60eaacb0c244a269e6d2235a4f0719a68e367805
unknown <binhpx92@gmai.com>
2023-02-09 04:59:58 +0700

    create header component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import ForgotPassWord from "./pages/ForgotPassWord";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header></Header>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
